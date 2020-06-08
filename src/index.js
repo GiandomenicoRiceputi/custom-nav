4f7fc8de4598c288790c90a72acbc22a08924af7
GiandoDev <giandomenicoriceputi@gmail.com>
2020-06-08 08:18:57 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Nav({children}) {
+    let items = React.Children.toArray(children)
+    for (let i = items.length - 1; i >= 1; i--) {
+        items.splice(i, 0, <span key={i} className="separator">|</span>)
+    }
+    return (
+        <nav className='nav'>
+            {items}
+        </nav>
+    )
+}
+
+function NavItem({children}) {
+    return (
+        <li className="menu-items">{children}</li>
+    )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Nav>
+            <NavItem><a href="/">Home</a></NavItem>
+            <NavItem><a href="/about">About</a></NavItem>
+            <NavItem><a href="contact">Contact</a></NavItem>
+        </Nav>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
